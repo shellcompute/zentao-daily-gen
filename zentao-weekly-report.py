12c3fe5c18bf2dee8b967d5a83751cc8ca38eafe
l2m2lq@gmail.com <114723704@qq.com>
2019-01-16 17:29:48 +0800

    upload.

--- zentao-weekly-report.py
+++ zentao-weekly-report.py
@@ -15,6 +15,7 @@ import decimal
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+import xlsxwriter
 
 __version__ = '0.5'
 
@@ -74,6 +75,7 @@ class ZentaoWeeklyReport:
       LEFT JOIN zt_task B ON A.task = B.id
       LEFT JOIN zt_project C ON B.project = C.id
       LEFT JOIN zt_module D ON B.module = D.id
+      WHERE B.status = 'closed'
       ORDER BY A.task ASC
       """
     start_date, end_date = self._previous_week_range(datetime.date.today())
@@ -95,12 +97,102 @@ class ZentaoWeeklyReport:
       conn.close()
     return rs
 
-  def _get_weekly_report_data(self):
-    pass
+  def _get_weekly_summary_data(self):
+    sql = """
+      SELECT
+      B.finishedBy AS finished_guy,
+      C.name AS sprint_title, 
+      D.name AS module_title, 
+      GROUP_CONCAT(DISTINCT B.closedBy SEPARATOR ',') AS closed_guy,
+      SUM(IF(estimate > 0, `estimate`, A.consumed)) AS consumed
+      FROM (
+        SELECT task, account, ROUND(SUM(consumed),1) AS consumed
+        FROM zt_taskestimate
+        WHERE date >= '{start_date}' AND date <= '{end_date}'
+        AND account IN ({users})
+        GROUP BY task, account
+      ) A
+      LEFT JOIN zt_task B ON A.task = B.id
+      LEFT JOIN zt_project C ON B.project = C.id
+      LEFT JOIN zt_module D ON B.module = D.id
+      WHERE B.status = 'closed'
+      GROUP BY finished_guy, sprint_title, module_title
+    """
+    start_date, end_date = self._previous_week_range(datetime.date.today())
+    sql = sql.format(users=','.join(self._weekly_report_users), start_date=start_date, end_date=end_date)
+
+    # Connect to the database
+    print("Connect to the database...")
+    conn = pymysql.connect(host=self._mysql_host,
+                           port=self._mysql_port,
+                           user=self._mysql_user,
+                           password=self._mysql_passwd,
+                           db='zentao',
+                           cursorclass=pymysql.cursors.DictCursor) 
+    try:   
+      with conn.cursor() as cursor:
+        cursor.execute(sql)
+        rs = cursor.fetchall()
+    finally:
+      conn.close()
+    return rs
 
   def gen_weekly_report(self):
+    # 查询汇总数据
+    summary_data = self._get_weekly_summary_data()
+    # 查询明细数据
     detail_data = self._get_weekly_detail_data()
-    print(detail_data)
+
+    # 写入Excel
+    start_date, end_date = self._previous_week_range(datetime.date.today())
+    xlsx_filename = '{start_date}至{end_date}项目组禅道数据.xlsx'.format(start_date=start_date, end_date=end_date)
+    
+    if os.path.exists(xlsx_filename):
+      os.remove(xlsx_filename)
+    
+    workbook = xlsxwriter.Workbook(xlsx_filename)
+    xlsx_title_format = workbook.add_format()
+    xlsx_title_format.set_bold() 
+    # 汇总数据页
+    worksheet = workbook.add_worksheet('汇总数据')
+    title = ['完成者', '所属迭代', '所属模块', '关闭人', '消耗工时']
+    title_width = [10, 30, 30, 15, 10]
+    for i in range(len(title)):
+      worksheet.write(0, i, title[i], xlsx_title_format)
+      worksheet.set_column(i, i, title_width[i])
+    row = 1
+    for i in summary_data:
+      worksheet.write(row, 0, i['finished_guy'])
+      worksheet.write(row, 1, i['sprint_title'])
+      worksheet.write(row, 2, i['module_title'])
+      worksheet.write(row, 3, i['closed_guy'])
+      worksheet.write(row, 4, i['consumed'])
+      row += 1
+
+    # 明细数据页
+    worksheet = workbook.add_worksheet('明细数据')
+    title = ['任务ID', '所属迭代', '所属模块', '任务名称', '需求ID','完成人', '关闭人', 
+      '完成时间', '关闭时间', '最初预计', '总消耗', '预计剩余']
+    for i in range(len(title)):
+      worksheet.write(0, i, title[i], xlsx_title_format)
+    row = 1
+    for i in detail_data:
+      worksheet.write(row, 0, i['task_id'])
+      worksheet.write(row, 1, i['sprint_title'])
+      worksheet.write(row, 2, i['module_title'])
+      worksheet.write(row, 3, i['task_title'])
+      worksheet.write(row, 4, i['story'])
+      worksheet.write(row, 5, i['finishedBy'])
+      worksheet.write(row, 6, i['closedBy'])
+      worksheet.write(row, 7, str(i['finishedDate']))
+      worksheet.write(row, 8, str(i['closedDate']))
+      worksheet.write(row, 9, i['estimate'])
+      worksheet.write(row, 10, i['consumed'])
+      worksheet.write(row, 11, i['left'])
+      row += 1
+    workbook.close()
+
+    # 发送邮件
     return True
 
 if __name__ == "__main__":
